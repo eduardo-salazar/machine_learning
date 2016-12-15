f1fce56e5ac266627e6481eaffc8db988eebac69
eduardo-salazar <esalazar922@gmail.com>
2016-12-15 21:13:13 +0800

    Added test for gain

--- spec/decision_trees_spec.rb
+++ spec/decision_trees_spec.rb
@@ -1,5 +1,20 @@
 require 'minitest/autorun'
 
 describe 'Decision Trees Behavior' do
+  before do
+    dt = DecisionTree.new(DECISIONTREE_TESTING_DATA)
+  end
 
+  it 'should calculate entropy' do
+    gain = dt.gain(2,3)
+    gain > 0
+  end
+
+  # it 'should return a decision tree' do
+  #   dt.wont_be_nil
+  # end
+  #
+  # it 'should return decision tree with node' do
+  #   dt.size > 0
+  # end
 end
