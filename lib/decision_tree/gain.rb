01eba3d950d03bbf21f66d92fdb8d52edd7665a1
eduardo-salazar <esalazar922@gmail.com>
2016-12-21 13:05:59 +0800

    Calculating entropy of target attribute

--- lib/decision_tree/gain.rb
+++ lib/decision_tree/gain.rb
@@ -3,6 +3,25 @@ module DecisionTree
 
   class Gain
 
+    # Calculate entropy of target
+    # information gain equals to entropy of target minus entropy of splitting the attribute
+    # ig = E(t) - E(T,X)
+    def information_gain(attr, labels)
+      e_target = entropy(attr,labels)
+      e_target
+    end
+
+    # To calculate entropy we have to check how many yes and how many no
+    def entropy(attr,labels)
+      # check binary attributes
+      mapped = parse_attr(attr)
+      if mapped.keys.size == 2
+        #Binary attributes
+        return gain(mapped.values[0],mapped.values[1])
+      end
+    end
+
+
     # Calculate the gain value
     def gain(positive, negative)
       sum = positive + negative
@@ -11,6 +30,18 @@ module DecisionTree
     end
 
     private
+    def parse_attr(array)
+      result = {}
+      array.each do |key|
+        if result.key?(key)
+          result[key] += 1
+        else
+          result[key] = 1
+        end
+      end
+      result
+    end
+
     def calcLog2(value)
       if(value <= 0)
         return 0.0
--- spec/gain_spec.rb
+++ spec/gain_spec.rb
@@ -3,14 +3,26 @@ require 'csv'
 require_relative '../lib/init.rb'
 
 
-describe 'Decision Trees Behavior' do
+describe 'Information Gain Behavior' do
+
+
   before do
-    @dt = DecisionTree::Gain.new
+    @data = [['N','Y','N','Y','Y'],['Y','Y','N','N','Y'],['Y','Y','Y','N','Y']]
+    @class_label = ['Y','Y','N','Y','Y']
+    @attributes = ['Has Credit Account','Reads Reviews','Is Previous Customer']
+
+    @gain = DecisionTree::Gain.new
   end
 
   it 'should calculate information gain' do
-    gain = @dt.gain(4,1)
+    gain = @gain.gain(4,1)
     gain.must_equal 0.722
+  end
+
+  # Test information gain of splitting Has Credit Account
+  it 'should calculate the information of splitting dimention[Has Credit Account]' do
+    information_gain = @gain.information_gain(@data[0],@class_label)
+    information_gain.must_equal 0.971
   end
 
 end
